f4ab1d5ba06e110d50126f0091ac568da4a64351
Sankalpa Adhikari <sankalpa2522@gmail.com>
2026-03-20 00:02:34 +0545

    chore: update astro to v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,44 +9,47 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://nsae.org.np",
-  integrations: [mdx(), sitemap(), react()],
-  vite: {
-    plugins: [tailwindcss(),],
-    build: {
-      rollupOptions: {
-        external: ['/pagefind/pagefind.js']
-      }
-    },
-    server: {
-      proxy: {
-        '/pagefind': {
-          target: 'http://localhost:1414',
-          changeOrigin: true,
-          configure: (proxy, options) => {
-            proxy.on('error', (err, req, res) => {
-              console.log('proxy error', err);
-            });
-            proxy.on('proxyReq', (proxyReq, req, res) => {
-              console.log('Proxying:', req.method, req.url);
-            });
-          }
-        }
-      }
-    }
-  },
-  prefetch: true,
-  redirects: {
-    "/blog": "/blog/page/1",
-    "/notices": "/notices/page/1",
-    "/careers": "/careers/page/1",
-    "/past-committee": "/past-committee/page/1",
-    "/resources": "/resources/page/1",
-    "/minutes": "/minutes/page/1",
-    "/events": "/events/page/1",
-  },
-  devToolbar: {
-    enabled:true,
-    placement: 'bottom-left',
-  },
+	site: "https://nsae.org.np",
+	image: {
+		domains: ["res.cloudinary.com", "cdn.prod.website-files.com"],
+	},
+	integrations: [mdx(), sitemap(), react()],
+	vite: {
+		plugins: [tailwindcss()],
+		build: {
+			rollupOptions: {
+				external: ["/pagefind/pagefind.js"],
+			},
+		},
+		server: {
+			proxy: {
+				"/pagefind": {
+					target: "http://localhost:1414",
+					changeOrigin: true,
+					configure: (proxy, options) => {
+						proxy.on("error", (err, req, res) => {
+							console.log("proxy error", err);
+						});
+						proxy.on("proxyReq", (proxyReq, req, res) => {
+							console.log("Proxying:", req.method, req.url);
+						});
+					},
+				},
+			},
+		},
+	},
+	prefetch: true,
+	redirects: {
+		"/blog": "/blog/page/1",
+		"/notices": "/notices/page/1",
+		"/careers": "/careers/page/1",
+		"/past-committee": "/past-committee/page/1",
+		"/resources": "/resources/page/1",
+		"/minutes": "/minutes/page/1",
+		"/events": "/events/page/1",
+	},
+	devToolbar: {
+		enabled: true,
+		placement: "bottom-left",
+	},
 });
