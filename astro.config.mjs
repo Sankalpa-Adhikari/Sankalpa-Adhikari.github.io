5d22d266577df1df759740b8cd4d0e8782885a30
Sankalpa Adhikari <sankalpa2522@gmail.com>
2026-05-05 21:07:20 +0545

    fix:tailwindcss import alias and update workflows versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,11 @@ export default defineConfig({
 	integrations: [mdx(), sitemap(), react()],
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@": "/src",
+			},
+		},
 		build: {
 			rollupOptions: {
 				external: ["/pagefind/pagefind.js"],
